ddafe084774c490a4240df207329a3bb349c87af
wilpet4 <william.petrik@gmail.com>
2022-05-30 16:16:39 +0200

    commit

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class CelestialObjectController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
